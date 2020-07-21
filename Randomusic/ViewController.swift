826dc5adc47e00ae7162ccd338412fbbe757b765
Priyank Shah <pcs36063@outlook.com>
2020-07-21 18:36:42 -0500

    First commit

---
+++ Randomusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randomusic
+//
+//  Created by Priyank Shah on 6/26/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
